7400c7f0e770a24c244b7a96e8c2d5735f2811b9
munjal-p <munjal.p@malayinfotech.com>
2025-03-31 17:13:20 +0530

    first commit

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "../screens/login";
+import Home from "../screens/Home";
+import Register from "../screens/Register";
+import Project from "../screens/Project";
+import UserAuth from "../auth/UserAuth";
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <UserAuth>
+              <Home />
+            </UserAuth>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<div>Logout</div>} />
+        <Route
+          path="/project"
+          element={
+            <UserAuth>
+              <Project />
+            </UserAuth>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
